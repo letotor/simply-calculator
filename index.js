cfc7c1ce1c5bb55969b4e956cb527ce8423b2609
victor DG <v.degermain@gmail.com>
2022-05-15 23:29:08 +0200

    RG ok

--- index.js
+++ index.js
@@ -1,95 +1,196 @@
-const msg= 
-`'                               ___'
-'      /|    //| |                 / /                                                             //   ) )'
-'     //|   // | |                / /      ___                   ___        ___        __         //            ___       //   __  ___     ___        ___       / __        ___'
-'    // |  //  | |  //   / /     / /     //   ) )   //   / /   //   ) )   //___) )   //  ) )     //           //   ) )   //     / /      //   ) )   //   ) )   //   ) )   //___) )'
-'   //  | //   | | ((___/ /     / /     //   / /   //   / /   //___/ /   //         //          //           //   / /   //     / /      //   / /   //         //   / /   //'
-'  //   |//    | |     / /     / /___  ((___/ /   ((___( (   //         ((____     //          ((____/ /    ((___( (   //     / /      ((___/ /   ((____     //   / /   ((____'
+const welcome = `╔═╗╔═╗████╔════╗████████████████████╔╗╔╗██████╔╗█████
+║║╚╝║║████╚══╗═║████████████████████║╠╝╚╗█████║║█████
+║╔╗╔╗╠╗█╔╗██╔╝╔╬══╦╗╔╦══╦══╦═╗╔══╦══╣╠╗╔╬══╦══╣╚═╦══╗
+║║║║║║║█║║█╔╝╔╝║╔╗║║║║╔╗║║═╣╔╝║╔═╣╔╗║║║║║╔╗║╔═╣╔╗║║═╣
+║║║║║║╚═╝║╔╝═╚═╣╚╝║╚╝║╚╝║║═╣║█║╚═╣╔╗║╚╣╚╣╚╝║╚═╣║║║║═╣
+╚╝╚╝╚╩═╗╔╝╚════╩══╩══╣╔═╩══╩╝█╚══╩╝╚╩═╩═╩══╩══╩╝╚╩══╝
+█████╔═╝║████████████║║██████████████████████████████
+█████╚══╝████████████╚╝██████████████████████████████
 `
-console.log(msg);
-let scrennLcd = document.getElementById("result");
 
+console.log(welcome);
+let scrennLcd = document.getElementById("result");
+let lcd=scrennLcd.innerText;
+let res=0;
+let calc=[];
+let lastNumber;
+let nums=[];
+let isOp=false;
+let isReset=false;
+
+
+// TODO next step clean code and factorisation en fonction
 
 /**Detect mouse input by clicking */
 const inputTouch=(e)=>{ 
     switch (e) {
         case 1:
              console.log("mouse un ", e)
-            scrennLcd.innerText += 1
+            isOp ? scrennLcd.innerText =1 : scrennLcd.innerText += 1
+            isOp=false;
+            calc.push(1);
+    
             break;
         case 2:
              console.log("mouse deux ", e)
-             scrennLcd.innerText += 2
+              isOp ? scrennLcd.innerText =2 : scrennLcd.innerText += 2
+             isOp=false;
+             calc.push(2);
             break;
         case 3:
              console.log("mouse trois ", e)
-             scrennLcd.innerText += 3
+              isOp ? scrennLcd.innerText =3 : scrennLcd.innerText += 3
+             isOp=false;
+              calc.push(3);
             break;
         case 4:
              console.log("mouse quatre ", e)
-             scrennLcd.innerText += 4
+            isOp ? scrennLcd.innerText =4 : scrennLcd.innerText += 4
+             isOp=false;
+             calc.push(4);
             break;
         case 5:
              console.log("mouse cinq ", e)
-             scrennLcd.innerText += 5
+              isOp ? scrennLcd.innerText =5 : scrennLcd.innerText += 5
+             isOp=false;
+             calc.push(5);
             break;
         case 6:
              console.log("mouse six ", e)
-             scrennLcd.innerText += 6
+              isOp ? scrennLcd.innerText =6 : scrennLcd.innerText += 6
+             isOp=false;
+             calc.push(6);
             break;
         case 7:
              console.log("mouse sept ", e)
-             scrennLcd.innerText += 7
+              isOp ? scrennLcd.innerText =7 : scrennLcd.innerText += 7
+             isOp=false;
+             calc.push(7);
             break;
         case 8:
              console.log("mouse huit ", e)
-             scrennLcd.innerText += 8
+              isOp ? scrennLcd.innerText =8 : scrennLcd.innerText += 8
+             isOp=false;
+             calc.push(8);
             break;
         case 9:
              console.log("mouse neuf ", e)
-             scrennLcd.innerText += 9
+              isOp ? scrennLcd.innerText =9 : scrennLcd.innerText += 9
+             isOp=false;
+             calc.push(9);
             break;
         case 0:
              console.log("mouse zero ", e)
-             scrennLcd.innerText += 0
+            isOp ? scrennLcd.innerText =0 : scrennLcd.innerText += 0
+             isOp=false;
+            calc.push(0);
             break;
         case ".":
             console.log("mouse decimale ", e)
-            scrennLcd.innerText += "."
+             isOp ? scrennLcd.innerText ="." : scrennLcd.innerText += "."
+            isOp=false;
+            calc.push(".");
         break;
         case "+":
              console.log(" mouse op plus ", e)
              scrennLcd.innerText = "+"
+            
+             
+            lastNumber = parseFloat(calc.join(''))
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+             nums.push("+");
+             calc=[];
+             
+            //  scrennLcd.innerText = res;
+             isOp=true;
+             
             break;
         case "-":
              console.log(" mouse op moins ", e)
              scrennLcd.innerText = "-"
+              isOp=true;
+
+               lastNumber = parseFloat(calc.join(''))
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+              nums.push("-");
+             calc=[];
+            
             break;
         case "*":
              console.log("mouse op multiply ",e)
              scrennLcd.innerText = "*"
+              isOp=true;
+                lastNumber = parseFloat(calc.join(''))
+                
+            
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+              nums.push("*");
+             calc=[];
             break;
         case "/":
              console.log("mouse op divided ", e)
              scrennLcd.innerText = "/"
+            
+              isOp=true;
+
+              lastNumber = parseFloat(calc.join(''))
+                
+            
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+              nums.push("/");
+             calc=[];
             break;
         case "%":
              console.log("mouse  op percent ", e)
              scrennLcd.innerText = "%"
+              isOp=true;
+              isOp=true;
+
+              lastNumber = parseFloat(calc.join(''))
+                
+            
+             nums.push(lastNumber*0.001);
+             res=eval(nums.join(''));
+             
+             
+             calc=[];
             break;
         case "sqrt":
              console.log("mouse  op sqrt ", e)
              scrennLcd.innerText = "sqrt"
+              isOp=true;
             break;
         
          case ("="):
-            console.log(" mouse OK ", e)
-            scrennLcd.innerText = "="
+             console.log("mouse op egqual ",e)
+             
+              isOp=true;
+
+              nums.push(parseFloat(calc.join('')));
+            res=eval(nums.join(''));
+
+            lastNumber = res
+             calc=[res];
+             nums=[];
+            scrennLcd.innerText = res
+             
+            
         break;
         
          case ("AC"):
             console.log("mouse Reset", e)
               scrennLcd.innerText = 0
+              isReset=true;
+              lastNumber=0;
+              res=[0];
+                calc=[];
+                nums=[];
+                isOp=false;
+
         break;
         
         default:
@@ -98,91 +199,202 @@ const inputTouch=(e)=>{
             break;
     }
 
-
+    //maj lcd global
      scrennLcd.innerText = detectFirstNumberZero(scrennLcd.innerText);
+    lcd=scrennLcd.innerText;
+    //    lastNumber = parseFloat(calc.join(''))
+
+   // RG01 supprimer si double operateur a la suite
+   
+     debug({lcd,lastNumber,nums,res,calc,isOp,isReset})
 }
 
 
+/** Fonction affichage debug */
+const debug=(obj)=>{
+console.log(obj)
+}
+
+let  = document.getElementById("result");
 
 
 
 /**Which touch is pressed on keyboard directly */
-document.onkeydown = checkKey;
-function checkKey(e) {
+document.onkeydown = (e) =>{
+
     e = e || window.event;
     switch (e.key) {
         case "1":
             //  console.log(e.key),
              console.log("touche un ", e.key)
-            scrennLcd.innerText += 1
+           isOp ? scrennLcd.innerText =1 : scrennLcd.innerText += 1
+            isOp=false;
+            calc.push(1);
             break;
         case "2":
              console.log("touche deux ", e.key)
-             scrennLcd.innerText += 2
+             isOp ? scrennLcd.innerText =2 : scrennLcd.innerText += 2
+             isOp=false;
+             calc.push(2);
             break;
         case "3":
              console.log("touche trois ", e.key)
-             scrennLcd.innerText += 3
+             isOp ? scrennLcd.innerText =3 : scrennLcd.innerText += 3
+             isOp=false;
+              calc.push(3);
             break;
         case "4":
              console.log("touche quatre ", e.key)
-             scrennLcd.innerText += 4
+             isOp ? scrennLcd.innerText =4 : scrennLcd.innerText += 4
+             isOp=false;
+             calc.push(4);
             break;
         case "5":
              console.log("touche cinq ", e.key)
-             scrennLcd.innerText += 5
+              isOp ? scrennLcd.innerText =5 : scrennLcd.innerText += 5
+             isOp=false;
+             calc.push(5);
             break;
         case "6":
              console.log("touche six ", e.key)
-             scrennLcd.innerText += 6
+              isOp ? scrennLcd.innerText =6 : scrennLcd.innerText += 6
+             isOp=false;
+             calc.push(6);
             break;
         case "7":
              console.log("touche sept ", e.key)
-             scrennLcd.innerText += 7
+             isOp ? scrennLcd.innerText =7 : scrennLcd.innerText += 7
+             isOp=false;
+             calc.push(7);
             break;
         case "8":
              console.log("touche hutit ", e.key)
-             scrennLcd.innerText += 8
+              isOp ? scrennLcd.innerText =8 : scrennLcd.innerText += 8
+             isOp=false;
+             calc.push(8);
             break;
         case "9":
              console.log("touche neuf ", e.key)
-             scrennLcd.innerText += 9
+             isOp ? scrennLcd.innerText =9 : scrennLcd.innerText += 9
+             isOp=false;
+             calc.push(9);
             break;
             
         case "0":
              console.log("touche zero ", e.key)
-             scrennLcd.innerText += 0
+             isOp ? scrennLcd.innerText =0 : scrennLcd.innerText += 0
+             isOp=false;
+            calc.push(0);
             break;
          case ".":
              console.log("touche decimale ", e.key)
-              scrennLcd.innerText += "."
+              isOp ? scrennLcd.innerText ="." : scrennLcd.innerText += "."
+            isOp=false;
+            calc.push(".");
             break;
         case "+":
              console.log("op plus ", e.key)
-             scrennLcd.innerText = "+"
+            scrennLcd.innerText = "+"
+            
+             
+            lastNumber = parseFloat(calc.join(''))
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+             nums.push("+");
+             calc=[];
+             
+            //  scrennLcd.innerText = res;
+             isOp=true;
             break;
         case "-":
              console.log("op moins ", e.key)
+              scrennLcd.innerText = "-"
+              isOp=true;
+
+               lastNumber = parseFloat(calc.join(''))
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+              nums.push("-");
+             calc=[];
             break;
         case "*":
              console.log("op multiply ", e.key)
+               scrennLcd.innerText = "*"
+              isOp=true;
+                lastNumber = parseFloat(calc.join(''))
+                
+            
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+              nums.push("*");
+             calc=[];
             break;
         case "/":
              console.log("op divided ", e.key)
+              scrennLcd.innerText = "/"
+            
+              isOp=true;
+
+              lastNumber = parseFloat(calc.join(''))
+                
+            
+             nums.push(lastNumber);
+             res=eval(nums.join(''));
+              nums.push("/");
+             calc=[];
             break;
         case "%":
              console.log("op percent ", e.key)
+             scrennLcd.innerText = "%"
+              isOp=true;
+              isOp=true;
+
+              lastNumber = parseFloat(calc.join(''))
+                
+            
+             nums.push(lastNumber*0.001);
+             res=eval(nums.join(''));
+             
+             
+             calc=[];
             break;
        
          case ("="):
             console.log("OK ", e.key)
+              isOp=true;
+
+              nums.push(parseFloat(calc.join('')));
+            res=eval(nums.join(''));
+
+            lastNumber = res
+             calc=[res];
+             nums=[];
+            scrennLcd.innerText = res
+             
         break;
          case ("Enter"):
-            console.log("OK", e.key)
+              isOp=true;
+
+              nums.push(parseFloat(calc.join('')));
+            res=eval(nums.join(''));
+
+            lastNumber = res
+             calc=[res];
+             nums=[];
+            scrennLcd.innerText = res
+             
         break;
          case ("Escape"):
                scrennLcd.innerText = 0
             console.log("Reset", e.key)
+           
+              isReset=true;
+              lastNumber=0;
+              res=[0];
+                calc=[];
+                nums=[];
+                isOp=false;
+
         break;
         
         default:
@@ -198,7 +410,7 @@ function checkKey(e) {
 
 /*** detect null before display */
 const detectFirstNumberZero = (str)=>{
-     if (str.length==2 && str.split('').includes("0") ){
+     if (str.length==2 && str[0]=="0" ){
         console.log("suppresion zero")
         let msgT = str.split('');
          str = msgT.slice(1).concat()
@@ -223,14 +435,19 @@ const isDisplayLong= ()=>{
 
 
 
-// function calculer(operateur, ...nombres) {
-//   let resultat = 0;
-//   if (operateur === '+') {
-//     for (let i = 0; i < nombres.length; i++) {
-//       resultat += nombres[i];
-//     }
-//   }
-// //   console.log(resultat);
-// }
+const  calculer= (operateur, ...nombres) =>{
+  let resultat = 0;
+  if (operateur === '+') {
+    for (let i = 0; i < nombres.length; i++) {
+      resultat += nombres[i];
+    }
+  }
+  if (operateur === '-') {
+    for (let i = 0; i < nombres.length; i++) {
+      resultat -= nombres[i];
+    }
+  }
+//   console.log(resultat);
+}
 
 // calculer('+', 42, 22, 12, 23); // 99
